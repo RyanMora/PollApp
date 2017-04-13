faf591a7ca86ce688a96369256af40cff9562d09
App Academy Student <student@appacademy.io>
2017-04-13 15:20:49 -0700

    before we destroy database

--- app/models/response.rb
+++ app/models/response.rb
@@ -18,14 +18,22 @@ class Response < ActiveRecord::Base
     foreign_key: :user_id,
     class_name: :User
 
-  belongs_to :question,
-    primary_key: :id,
-    foreign_key: :question_id,
-    class_name: :Question
+  # belongs_to :question,
+  #   primary_key: :id,
+  #   foreign_key: :question_id,
+  #   class_name: :Question
 
   belongs_to :answer_choice,
     primary_key: :id,
     foreign_key: :answer_choice_id,
     class_name: :AnswerChoice
+
+  has_one :question,
+    through: :answer_choice,
+    source: :question
+
+  def sibling_responses
+    question.responses
+  end
 
 end
